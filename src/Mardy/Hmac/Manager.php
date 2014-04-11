913d8b5054e5d430301407ae38bd94609be23102
Michael Bardsley <me@mic-b.co.uk>
2014-04-11 22:19:50 +0100

    Complete refactor of v1 this is the new manager

--- src/Mardy/Hmac/Manager.php
+++ src/Mardy/Hmac/Manager.php
@@ -2,295 +2,152 @@
 
 namespace Mardy\Hmac;
 
-use Mardy\Hmac\Headers\Values;
-use Mardy\Hmac\Config\Config;
-use Mardy\Hmac\Storage\NonPersistent;
+use Mardy\Hmac\Adapters\AdapterInterface;
+use Mardy\Hmac\Entity;
 
 /**
- * Hmac Class
+ * Manager Class
  *
  * Manages all the HMAC checking for the application
  *
- * @package        mardy-dev
+ * @package        mardy-git
  * @subpackage     Authentication
  * @category       HMAC
- * @author         Michael Bardsley
+ * @author         Michael Bardsley <me@mic-b.co.uk>
  */
-class Hmac
+class Manager
 {
     /**
-     * @var \Mardy\Hmac\Config\Config
+     * @var \Mardy\Hmac\Adapter\AdapterInterface
      */
-    protected $config;
+    protected $adapter;
 
     /**
-     * @var \Mardy\Hmac\Storage\NonPersistent
+     * @var \Mardy\Hmac\Entity
      */
-    protected $storage;
+    protected $entity;
 
     /**
-     * @var \Mardy\Hmac\Headers\Values
-     */
-    protected $headerValues;
-
-    /**
-     * Hold the error message
+     * Number of seconds the key will remain active for
      *
-     * @var string
+     * @var int
      */
-    protected $error = '';
+    protected $ttl = 2;
 
     /**
      * Constructor
      *
-     * @param \Mardy\Hmac\Config\Config $config
-     * @param \Mardy\Hmac\Storage\NonPersistent $storage
-     * @param \Mardy\Hmac\Headers\HeaderValues $headerValues
+     * @param \Mardy\Hmac\Adapters\AdapterInterface $adapter
      */
-    public function __construct(Config $config, NonPersistent $storage, Values $headerValues)
+    public function __construct(AdapterInterface $adapter)
     {
-        $this->setConfig($config);
-        $this->setStorage($storage);
-        $this->setHeaderValues($headerValues);
+        $this->adapter = $adapter;
+        $this->entity = new Entity;
+        $this->adapter->setEntity($this->entity);
+    }
+
+    /**
+     * Sets the private key in the item
+     *
+     * @param string $key
+     * @return \Mardy\Hmac\Manager
+     */
+    public function key($key)
+    {
+        $this->entity->setKey($key);
+
+        return $this;
+    }
+
+    /**
+     * Sets the time in the item
+     *
+     * @param int $time
+     * @return \Mardy\Hmac\Manager
+     */
+    public function time($time)
+    {
+        $this->entity->setTime($time);
+
+        return $this;
+    }
+
+    /**
+     * Sets the data in the item
+     *
+     * @param string $data
+     * @return \Mardy\Hmac\Manager
+     */
+    public function data($data)
+    {
+        $this->entity->setData($data);
+
+        return $this;
+    }
+
+    /**
+     * Sets the adapter config
+     *
+     * @param array $config
+     * @return \Mardy\Hmac\Manager
+     */
+    public function config(array $config)
+    {
+        $this->adapter->setConfig($config);
+
+        return $this;
     }
 
     /**
      * Checks the HMAC key to make sure it is valid
      *
-     * @throws HmacException
-     * @return bool true if it ok and false if it fails
+     * @return boolean
      */
-    public function check()
+    public function isValid($hmac)
     {
-        //intial checks - checking the HMAC as well
-        if (! $this->checkInput(true)) {
-            return false;
-        }
+        $this->adapter->encode();
 
-        //work out how long the request has taken by subtracting the $ts from the current time
-        $taken = time() - $this->getStorage()->getTimestamp();
-
-        //check to make sure the request was sent within the last 2 mins, if not show an exception
-        if ($taken > $this->getConfig()->getValidityPeriod()) {
-            $this->setError("The request has taken to long");
-            return false;
-        }
-
-        //all the values have been correctly set, now we need to encode the HMAC to see if it matches
-        //to the one that was sent in the request
-        $hmac = $this->encode();
-
-        //if the stored hmac and encoded HMCA don't match up return false
-        if ($hmac != $this->getStorage()->getHmac()) {
-            $this->setError("HMAC is invalid");
-            return false;
-        }
-
-        //return true because the HMAC matches
-        return true;
-    }
-
-    /**
-     * Creates an HMAC key using the supplied parameters
-     *
-     * @return boolean|string contains false or an array with the HMAC details
-     */
-    public function create()
-    {
-        //intial checks - without checking the HMAC key
-        if (! $this->checkInput(false)) {
-            return false;
-        }
-
-        $taken = time() - $this->getStorage()->getTimestamp();
-
-        if ($taken > $this->getConfig()->getValidityPeriod()) {
-            $this->setError("The request has taken to long");
-            return false;
-        }
-
-        $hmac = $this->encode();
-
-        return $this->headerValues->setKey($hmac)
-                                  ->setWhen($this->getStorage()->getTimestamp())
-                                  ->setUri($this->getStorage()->getUri())
-                                  ->setDisplayPrefix(false);
-    }
-
-    /**
-     * Setter method to set the config object
-     *
-     * @param ConfigValues $config
-     */
-    public function setConfig(Config $config)
-    {
-        $this->config = $config;
-
-        return $this;
-    }
-
-    /**
-     * Getter method to return the config object
-     *
-     * @return ConfigValues
-     */
-    public function getConfig()
-    {
-        return $this->config;
-    }
-
-    /**
-     * Setter method to set the storage object
-     *
-     * @param NonPersistent $storage
-     * @return Hmac
-     */
-    public function setStorage(NonPersistent $storage)
-    {
-        $this->storage = $storage;
-
-        return $this;
-    }
-
-    /**
-     * Getter method to return the storage object
-     *
-     * @return NonPersistent
-     */
-    public function getStorage()
-    {
-        return $this->storage;
-    }
-
-    /**
-     * @param \Mardy\Hmac\Headers\HeaderValues $storage
-     * @return \Mardy\Hmac\Hmac
-     */
-    public function setHeaderValues($headerValues)
-    {
-        $this->headerValues = $headerValues;
-
-        return $this;
-    }
-
-    /**
-     * @return \Mardy\Hmac\Headers\HeaderValues
-     */
-    public function getHeaderValues()
-    {
-        return $this->headerValues;
-    }
-
-    /**
-     * Setter method to set the error message
-     *
-     * @param string $error
-     * @return Hmac
-     */
-    public function setError($error)
-    {
-        $this->error = $error;
-
-        return $this;
-    }
-
-    /**
-     * Getter method to return the error message
-     *
-     * @return string $error
-     */
-    public function getError()
-    {
-        return $this->error;
-    }
-
-    /**
-     * Encodes the HMAC based on the values that have been entered
-     *
-     * @return string
-     */
-    protected function encode()
-    {
-        //the first has contains the URI and timestamps that have been set
-        $firsthash = hash(
-            $this->getConfig()->getAlgorithm(),
-            $this->getStorage()->getUri() . "@" . $this->getStorage()->getTimestamp()
-        );
-
-        //loop to make it hard to crack this hash
-        for ($i = 0; $i < 10; $i++) {
-            $firsthash = hash(
-                $this->getConfig()->getAlgorithm(),
-                $firsthash
-            );
-        }
-
-        //the second has the private key
-        $secondhash = hash(
-            $this->getConfig()->getAlgorithm(),
-            $this->getConfig()->getKey()
-        );
-
-        //loop to make it hard to crack this hash
-        for ($i = 0; $i < 10; $i++) {
-            $secondhash = hash(
-                $this->getConfig()->getAlgorithm(),
-                $secondhash
-            );
-        }
-
-        //returned is an hash of both the previous hashes
-        $finalhash = hash(
-            $this->getConfig()->getAlgorithm(),
-            $firsthash . "-" . $secondhash
-        );
-
-        //loop to further encode the HMAC key, this will make it harder to crack
-        for ($i = 0; $i < 100; $i++) {
-            $finalhash = hash(
-                $this->getConfig()->getAlgorithm(),
-                $finalhash
-            );
-        }
-
-        //returned is an hash that has been hashed a lot of times
-        return $finalhash;
-    }
-
-    /**
-     * Check the inputs that have been supplied
-     * HMAC
-     * URI
-     * Timestamp
-     *
-     * @param boolean false if the HMAC needs to the checked or true if not
-     * @return boolean true if all the values have been sent false if not
-     */
-    protected function checkInput($checkHmac = true)
-    {
-        //return false if the private key is is null or has not been set
-        if (is_null($this->getConfig()->getKey()) || $this->getConfig()->getKey() == '') {
-            $this->setError("No private key has been set");
-            return false;
-        }
-
-        //return false if the $hmac is null
-        if ($checkHmac === true && is_null($this->getStorage()->getHmac())) {
-            $this->setError("An attempt to assign a null HMAC key was detected");
-            return false;
-        }
-        //return false if the $uri is null
-        if (is_null($this->getStorage()->getUri())) {
-            $this->setError("No URI was set when an HMAC check was attempted");
-            return false;
-        }
-
-        //return false if the ts has not been set
-        if (is_null($this->getStorage()->getTimestamp())) {
-            $this->setError("No TimeStamp was set when an HMAC check was attempted");
+        if (((time() - $this->entity->getTime()) >= $this->ttl && $this->ttl != 0) ||
+            ($hmac != $this->entity->getHmac())
+        ) {
             return false;
         }
 
         return true;
+    }
+
+    /**
+     * Encodes the HMAC and returns an array
+     *
+     * @return array
+     */
+    public function encode()
+    {
+        $this->adapter->encode();
+
+        return $this;
+    }
+
+    /**
+     * Returns the entity array or false if the hmac doesn't exist
+     *
+     * @return type
+     */
+    public function toArray()
+    {
+        return $this->entity->toArray();
+    }
+
+    /**
+     * Sets the Time To Live
+     *
+     * @param int $ttl
+     * @return \Mardy\Hmac\Manager
+     */
+    public function ttl($ttl)
+    {
+        $this->ttl = (int) $ttl;
+
+        return $this;
     }
 }
